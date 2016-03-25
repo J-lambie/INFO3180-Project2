00f95f3b9699b3268f0596b9fecaf52fbdcb0137
j_lambie <jillianjohnson9957@gmail.com>
2016-03-25 18:00:46 +0000

    Started models for databases

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from . import db  
+class User(db.Model):   
+    __tablename__='user'
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80))
+    first_name = db.Column(db.String(80))     
+    last_name = db.Column(db.String(80)) 
+    image=db.Column(db.String(80))
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.first_name)
+        
+class Wishlist(db.Model):
+    __tablename__='wishlist'
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user_id')
+    title=db.Column(db.String(80))
+    url=db.Column(db.String(100))
+    description=db.Column(db.String(250))
+    img_url=db.Column(db.String(100))
+    
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
